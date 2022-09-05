29d2ed45ec183bc86197994dce5b0d117227770c
Williamjacobsen <71521465+Williamjacobsen@users.noreply.github.com>
2022-09-05 22:04:07 +0200

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Axios from "axios";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Navigate,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import StartingPage from "./pages/startingPage/startingPage";
 import Home from "./pages/Home/Home";
@@ -9,17 +14,12 @@ import "./App.css";
 
 function App() {
   /* eslint-disable */
-
   const [loginStatus, setLoginStatus] = useState(false);
 
   Axios.defaults.withCredentials = true;
-
   useEffect(() => {
-    // re-ender twice cuz of react stictMode (not in production only development)
     Axios.get("http://localhost:5000/LogInGet").then((response) => {
-      // console.log(response);
       if (response.data.loggedIn === true) {
-        // setLoginStatus(response.data.user[0].username);
         setLoginStatus(true);
       }
     });
@@ -29,6 +29,9 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          {loginStatus ? (
+            <Route path="/" element={<Navigate replace to="/home" />} />
+          ) : null}
           <Route path="/" element={<StartingPage />}></Route>
           <Route element={<ProtectedRoute />}>
             <Route element={<Home />} path="/home" exact></Route>
